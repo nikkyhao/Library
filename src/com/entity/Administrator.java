e32c1331e622a2340b6155878b10f7f2fca08584
714285 <bnm852@outlook.com>
2016-07-05 16:18:27 +0800

    util0.1版本

--- src/com/entity/Administrator.java
+++ src/com/entity/Administrator.java
@@ -5,6 +5,7 @@ public class Administrator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}  
+	
 
 }
---
+++ src/com/util/Utils.java
@@ -0,0 +1,27 @@
+package com.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+/**
+ * 包含4个方法：生成借阅证，生成条形码，扫描条形码，批量导入数据库
+ * @author YSJ
+ * @version 0.8
+ * xlsToDB(需要jar包poi开头的包*6个;xmlbeans;)//
+ * 
+ */
+public class Utils {
+	
+	
+	
+	
+
+}
