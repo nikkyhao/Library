2f43a1d20e2fc23ca35d7bf9a748123ff74f9c2a
nikkyhao <930334832@qq.com>
2016-07-06 10:15:34 +0800

    修改了各种地方

--- src/com/entity/Administrator.java
+++ src/com/entity/Administrator.java
@@ -1,13 +1,444 @@
 package com.entity;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-
+import com.test.LibConnection;
+/**
+ * 
+ * @author ZM
+ *
+ */
 public class Administrator {
 	
-//插入单个用户
-public void insertUser(){
+	Statement sta = LibConnection.getStatement();	
+	public void insertUser(String name, int ID, String sex, int stuID, String type, String tel, int pwd) {
+		// 插入单个用户
+		Date start = new Date(System.currentTimeMillis());// 借阅证日期，系统当前日期
+		Date end = new Date(start.getYear() + 1, start.getMonth(), start.getDay());// 失效日期，过一年
+
+		String sql = "INSERT INTO `user`(NAME,ID,sex,TypeOfCard,starttime,per_of_val,studentID,telephone,password)"
+				+ "VALUES('" + name + "'," + ID + ",'" + sex + "','" + type + "','" + start + "','" + end + "'," + stuID
+				+ ",'" + tel + "'," + pwd + ")";
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void updateUser(String name, int ID, String sex, int stuID, String type, String tel, double money,
+			int cardID) {
+		String sql = "update user set NAME='" + name + "',ID=" + ID + ",sex='" + sex + "',TypeOfCard='" + type
+				+ "',studentID=" + stuID + ",telephone=" + tel + ",money_Reserved=" + money + " where cardID=" + cardID;
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 	
-}
+	public void updateUser(double money,int cardID) {
+		//修改预存金额
+		String sql = "update user set money_Reserved=" + money + " where cardID=" + cardID;
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
 
+	public ResultSet searchUser(int cardID) {
+		// 通过借阅证卡号查询用户
+		String sql = "SELECT cardID,`name`,sex,TypeOfCard,money_Reserved,per_of_val FROM `user` WHERE cardID=" + cardID;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public void insertBooktype(String name, String author, String press, String type, Date pressdate, String edition,
+			String presstime, String size, int pages, double price) {
+		// 插入单本图书类别
+
+		String sql = "INSERT INTO `category` (bookname,author,press,booktype,pressdate,edition,presstime,size,pages,price)"
+				+ "VALUES('" + name + "','" + author + "','" + press + "','" + type + "','" + pressdate + "','"
+				+ edition + "','" + presstime + "','" + size + "'," + pages + "," + price + ")";
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void insertBook(int bookid, int index) {
+		// 添加图书，bookid是条形码
+		String sql = "insert into book values(" + bookid + "," + index + ")";
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void updateBook(String name, String author, String press, String type, double price, int index) {
+		// 修改图书信息
+		String sql = "update category set bookname='" + name + "',author='" + author + "',press='" + press
+				+ "',booktype='" + type + "', price=" + price + " where `index`=" + index;
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void borrow(String bookid, int cardid) {
+		// 借书,是否要判断这本书能不能借？如果在borrow表中，归还日期为null，则不能借
+		Date bo_date = new Date(System.currentTimeMillis());// 借阅日期，系统当前日期
+		Date dead = new Date(bo_date.getYear(), bo_date.getMonth() + 1, bo_date.getDay());// 截止日期，过一个月
+		String sql = "insert into borrow (bo_bookid,bo_cardID,bo_date,deadline) values('" + bookid + "'," + cardid
+				+ ",'" + bo_date + "','" + dead + "')";
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+//	public boolean canborrow(String bookid) {
+//		// 判断这本书能不能借
+//		boolean canborrow = true;
+//		String sql = "select return_date from borrow where bo_bookid='" + bookid + "'";
+//		ResultSet rs = null;
+//		try {
+//			rs = sta.executeQuery(sql);
+//			while (rs.next()) {
+//				String ret_date = rs.getString("return_date");
+//				if (ret_date == null) {
+//					canborrow = false;
+//					return canborrow;
+//				}
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return canborrow;
+//	}
+	public int seachNum(int index) {
+		// 一共有几本index类的书
+		int num = 0;
+		String sql = "SELECT COUNT(bookid) as num FROM book,category,borrow WHERE bookid=bo_bookid AND cateindex=`index` AND return_date IS NULL AND `index`=" + index;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+			while (rs.next()) {
+				num = rs.getInt("num");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return num;
+	}
+	public int seachBorrowNum(int index) {
+		// 一共借出了几本index类的书
+		int num = 0;
+		String sql = "select count(bookid) as num from category,book where `index`=cateindex and `index`=" + index;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+			while (rs.next()) {
+				num = rs.getInt("num");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return num;
+	}
+
+	public void back(String bookid, int cardid) {
+		// 还书
+		Date back = new Date(System.currentTimeMillis());// 归还日期，系统当前日期
+		String sql = "update borrow set return_date='" + back + "' where bo_bookid='" + bookid + "' and bo_cardID="
+				+ cardid;
+		try {
+			sta.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet searchHisBor() {
+		// 查询所有历史借阅记录
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID";
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public ResultSet searchCurBor() {
+		// 查询所有当前借阅
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID AND return_date IS NULL";
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public ResultSet searchHisBorByBook(int index) {
+		// 查询每本书的借阅记录
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID and `index`="
+				+ index;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public ResultSet searchHisBorByUser(int cardID) {
+		// 查询每个人的借阅记录
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID and cardID="
+				+ cardID;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public ResultSet searchOverdate() {
+		// 查询所有违章记录，归还日期为空并且deadline小于当前日期，或者，归还日期大于deadline
+		Date date = new Date(System.currentTimeMillis());//系统当前日期
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID and (return_date > deadline or (deadline<'"+date+"' and return_date is null))";
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public ResultSet searchOverdateByUser(int cardID) {
+		// 查询某个人的违章记录
+		Date date = new Date(System.currentTimeMillis());//系统当前日期
+		String sql = "SELECT bookid,bookname,cardID,`name`,bo_date,deadline,return_date FROM borrow,book,`user`,category WHERE bo_bookid=bookid AND `index`=cateindex AND bo_cardID=cardID and (return_date > deadline or (deadline<'"+date+"' and return_date is null))"+" and cardID="
+				+ cardID;
+		ResultSet rs = null;
+		try {
+			rs = sta.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public double fine(){
+		//超期天数
+		double fine=0;
+		Date date = new Date(System.currentTimeMillis());//系统当前日期
+		String sql = "SELECT deadline,return_date FROM borrow WHERE return_date > deadline";
+		String sql2="select deadline from borrow where (deadline<'"+date+"' and return_date is null)";
+		ResultSet rs = null;
+		ResultSet rs1 = null;
+		try {
+			rs = sta.executeQuery(sql);		
+			while(rs.next()){
+				Date deadline=rs.getDate("deadline");
+				Date return_date=rs.getDate("return_date");
+				long beginTime = deadline.getTime(); 
+				long endTime = return_date.getTime(); 
+				long betweenDays = (long)((endTime - beginTime) / (1000 * 60 * 60 *24) + 0.5); 
+				System.out.println(betweenDays);	
+			}
+			rs1=sta.executeQuery(sql2);
+			while(rs1.next()){
+				Date deadline2=rs1.getDate("deadline");
+				long dead=deadline2.getTime();
+				long today=date.getTime();
+				long betweenDays = (long)((today - dead) / (1000 * 60 * 60 *24) + 0.5); 
+				System.out.println(betweenDays);	
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return fine;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Administrator admin = new Administrator();
+		String name = "bob";
+		int ID = 19951016;
+		String sex = "man";
+		int stuID = 20130530;
+		String type = "stu";
+		String tel = "15165110710";
+		int pwd = stuID;
+		double money = 100;
+		int cardID = 668;
+
+		// 测试添加会员方法
+		// admin.insertUser(name, ID, sex, stuID, type, tel, pwd);
+		// System.out.println("添加成功");
+
+		// 测试修改会员方法
+		// admin.updateUser(name, ID, sex, stuID, type, tel,money, cardID);
+		// System.out.println("修改成功");
+
+		// 测试搜索会员方法
+		// ResultSet rs = admin.searchUser(cardID);
+		// try {
+		// while (rs.next()) {
+		// String cID = rs.getString("cardID");
+		// String username = rs.getString("name");
+		// String date = rs.getString("per_of_val");
+		// System.out.println(username + "," + date);
+		// }
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// 测试添加类别的方法
+		String bname = "java2";
+		String author = "ABCD";
+		String press = "SDDX";
+		String booktype = "IT";
+		Date date = new Date(System.currentTimeMillis());
+		String edition = "3";
+		String presstime = "2";
+		String size = "16";
+		int pages = 200;
+		double price = 50;
+		int index = 121322;
+		// admin.insertBooktype(bname, author, press, booktype,date, edition,
+		// presstime, size, pages, price);
+
+		// 测试添加单本图书
+		// admin.insertBook(0002,121322);
+
+		// 测试修改图书信息
+		// admin.updateBook(name, author, press, type, price, index);
+
+		// 测试借书功能
+		// admin.borrow("001", 669);
+
+		// 测试该书能不能借
+		// boolean b=admin.canborrow("12333");
+		// System.out.println(b);
+
+		int num=admin.seachNum(121322);
+		System.out.println(num);
+		// 测试还书功能
+		// admin.back("001", 668);
+
+		// 测试查询所有借阅记录
+		// ResultSet rs=admin.searchHisBor();
+		// try {
+		// while(rs.next()){
+		// String bid=rs.getString("bookname");
+		// String cid=rs.getString("name");
+		// System.out.println(bid+","+cid);
+		// }
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// 测试查询当前借阅记录
+		// ResultSet rs=admin.searchCurBor();
+		// try {
+		// while(rs.next()){
+		// String bid=rs.getString("bookname");
+		// String cid=rs.getString("name");
+		// System.out.println(bid+","+cid);
+		// }
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// 测试查询某本书的借阅记录
+		// ResultSet rs=admin.searchHisBorByBook(index);
+		// try {
+		// while(rs.next()){
+		// String bid=rs.getString("bookname");
+		// String cid=rs.getString("name");
+		// System.out.println(bid+","+cid);
+		// }
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// 测试查询某个人的借阅记录
+		// ResultSet rs=admin.searchHisBorByUser(cardID);
+		// try {
+		// while(rs.next()){
+		// String bid=rs.getString("bookname");
+		// String cid=rs.getString("name");
+		// System.out.println(bid+","+cid);
+		// }
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// 测试查询某个人的违章记录
+//		 ResultSet rs=admin.searchOverdate();
+//		 try {
+//		 while(rs.next()){
+//		 String bid=rs.getString("bookname");
+//		 String cid=rs.getString("name");
+//		 System.out.println(bid+","+cid);
+//		 }
+//		 } catch (SQLException e) {
+//		 // TODO Auto-generated catch block
+//		 e.printStackTrace();
+//		 }
+
+		//超期天数
+//		double fine=admin.fine();
+//		System.out.println(fine);
+	}
 }
--- src/com/entity/User.java
+++ src/com/entity/User.java
@@ -6,20 +6,18 @@ package com.entity;
 import java.sql.*;
 import java.util.Calendar;
 
-import com.mysql.jdbc.Statement;
-
+import com.test.LibConnection;
+import java.sql.*;
 
 public class User {
-	static Connection con = null;
 	protected static int cardID;
+	Statement statement = LibConnection.getStatement();
 	
 	public User(int cardID){
 		this.cardID = cardID;
 	}
-	
-	public static ResultSet showMyBook_now() throws SQLException{		//��ѯ��ǰ���ģ�����������ţ��������ڣ�
+	public ResultSet showMyBook_now() throws SQLException{		//��ѯ��ǰ���ģ�����������ţ��������ڣ�
 		String sql = "SELECT bookid,bookname,bo_date,deadline FROM borrow,category JOIN book ON category.`index`=book.cateindex WHERE (bo_bookid,bookname) IN (select bookid,bookname from category JOIN book ON category.`index`=book.cateindex WHERE bookid in(select bo_bookid from borrow WHERE bo_cardID=123)) and return_date is null AND bo_cardID="+cardID;
-		java.sql.Statement statement =con.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 		return rs;
 	}
@@ -35,43 +33,37 @@ public class User {
 		
 	}
 	
-	public static ResultSet showMyBook_history() throws SQLException{		//��ѯ��ʷ���ģ�����������ţ��������ڣ�
+	public ResultSet showMyBook_history() throws SQLException{		//��ѯ��ʷ���ģ�����������ţ��������ڣ�
 		String sql = "SELECT bookid,bookname,bo_date,return_date FROM borrow,category JOIN book ON category.`index`=book.cateindex WHERE (bo_bookid,bookname) IN (select bookid,bookname from category JOIN book ON category.`index`=book.cateindex WHERE bookid in(select bo_bookid from borrow WHERE bo_cardID=123)) and return_date is not null and bo_cardID = "+cardID;
-		java.sql.Statement statement =con.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 		return rs;
 	}
 	
-	public static ResultSet show_my_info() throws SQLException{		//��ѯ��ǰ�û���������Ϣ
+	public ResultSet show_my_info() throws SQLException{		//��ѯ��ǰ�û���������Ϣ
 		String sql = "select * from user where cardID ="+cardID;
-		java.sql.Statement statement =con.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 		return rs;
 	}
 	
 	
-	public static void Update_info(String propertyname,String value) throws SQLException{		//�����û�����
+	public void Update_info(String propertyname,String value) throws SQLException{		//�����û�����
 		String sql = "update user set "+propertyname+" = "+value+" where cardID = "+cardID;
-		java.sql.Statement statement =con.createStatement();
 		statement.executeUpdate(sql);	
 	}
 	
-	public static ResultSet showMyBook_Reminder() throws SQLException{		//�߻����������뻹�����ڻ�������ģ�����������ţ��������ڣ�
+	public ResultSet showMyBook_Reminder() throws SQLException{		//�߻����������뻹�����ڻ�������ģ�����������ţ��������ڣ�
 		String sql = "select a.bookname,a.`index`,r.bo_date,r.return_date from category a,book b,borrow r  where DATEDIFF(r.return_date,now())<3 and DATEDIFF(r.return_date,now())>0 and  a.`index` = b.cateindex and b.bookid = r.bo_bookid and bo_cardID= "+cardID;
-		java.sql.Statement statement =con.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 		return rs;
 	}
 	
-	public static void Alter_order(int Order_index) throws SQLException{		//���һ��ԤԼ��Ϣ
+	public void Alter_order(int Order_index) throws SQLException{		//���һ��ԤԼ��Ϣ
 		String sql = "insert into `order` values("+Order_index+","+cardID+",now(),(select date_add(now(), interval 1 day)))";
-		java.sql.Statement statement =con.createStatement();
 		statement.executeUpdate(sql);
 	}
 	
-	public static ResultSet showMyorder() throws SQLException{		//��ʾԤԼ��Ϣ
+	public ResultSet showMyorder() throws SQLException{		//��ʾԤԼ��Ϣ
 		String sql = "select a.bookname,a.`index`,b.bookingdate,b.deadline from category a,book c,`order` b,user u where a.`index`=c.cateindex and c.bookid=b.ord_bookid and b.ord_cardID=u.cardID and cardId = "+cardID;
-		java.sql.Statement statement =con.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 		return rs;
 	}
@@ -79,19 +71,10 @@ public class User {
 	
 
 	public static void main(String args[]) throws SQLException{
-		try {
-			Class.forName("com.mysql.jdbc.Driver");// ����Mysql��������
-			
-			con = DriverManager.getConnection(
-					"jdbc:mysql://127.0.0.1:3306/test", "root", "wanner1597");// ������������	
-			System.out.println("hello");
-		} catch (Exception e) {
-			System.out.println("���ݿ�����ʧ��" + e.getMessage());
-		}
 		
-		/////�������
 		cardID = 223;
-		ResultSet rs = User.showMyorder();
+		User user = new User(cardID);
+		ResultSet rs = user.showMyorder();
 		String bookname = null;
 		try {
 			while (rs.next()) {
